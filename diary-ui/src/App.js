a47b8c41e3b5bfd335ea60e9483d90b0f584b5a2
Vishnu Rana <mvishnurana@gmail.com>
2022-04-09 17:46:37 +0800

    Generated new react app

---
+++ diary-ui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Notes } from './components/Notes/notes';
+
+function App() {
+  return (
+    <div className='p-2'>
+        <Notes />
+    </div>
+  );
+}
+
+export default App;
---
+++ diary-ui/src/components/Notes/notes.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './notes.scss';
+
+export function Notes() {
+    const [content, setContent] = useState('');
+    const [post, setPost] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
+
+    return (
+        <>
+            <h1>Notes App</h1>
+            <div className="notes-layout">
+                <div>
+                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                </div>
+
+                <textarea 
+                    className='textArea' 
+                    rows={15} 
+                    onChange={(e) => setContent(e.target.value)}
+                    value={content} 
+                />
+            </div>
+
+            <button 
+                className='save-button'
+                variant="outline-primary"
+                onClick={() => setPost(!post)}
+                disabled={content.length === 0}> 
+                SAVE
+            </button>
+        </>
+    )
+}
