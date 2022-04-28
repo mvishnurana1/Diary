8faa8afff8f6f84ad33b27e8c9de00981831f553
Vishnu Rana <mvishnurana@gmail.com>
2022-04-28 09:13:05 +0800

    Added spinner to the notes component

--- diary-ui/src/components/Notes/notes.jsx
+++ diary-ui/src/components/Notes/notes.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
 import axios from 'axios';
-import DatePicker from "react-datepicker";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import { dateFormat } from '../../helper/date-fn';
 import './notes.scss';
 
 export function Notes() {
+    const BASE_URL = 'https://localhost:44315/';
+
     const [content, setContent] = useState('');
-    const [post, setPost] = useState(false);
     const [startDate, setStartDate] = useState(new Date());
     const [displaySearch, setDisplaySearch] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [searchedContent,  setSearchedContent] = useState('');
 
     function displaySearchBox() {
@@ -26,17 +30,36 @@ export function Notes() {
     }
 
     function getDate(date) {
-        fetchData(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+        const formattedDate = dateFormat(date);
+        fetchDataByDate(formattedDate);
     }
 
-    function fetchData(date) {
-        axios.get(`https://localhost:44315/get/${date}`)
+    function fetchDataByDate(date) {
+        axios.get(`${BASE_URL}get/${date}`)
             .then((val) => {
                 if (val.data.length === 0) {
                     setContent('');
                 } else {
                     setContent(val.data[0].content);
                 }
+            })
+            .catch(() => {
+                setContent('Something went wrong...');
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }
+
+    function postNewNotes() {
+        const newNote = {
+            'submittedDateTime': startDate.toISOString(),
+            'Content': content
+        };
+
+        axios.post(`${BASE_URL}post`, newNote)
+            .then(() => {
+                setContent(newNote.Content);
             });
     }
 
@@ -65,25 +88,29 @@ export function Notes() {
                         onChange={(date) => {
                             setStartDate(date);
                             getDate(date);
+                            setLoading(true);
                         }}
                     />
                 </div>
 
-                <textarea
+            {loading 
+                ? <ClipLoader color='red' size={150} />
+                : <textarea
                     className='textArea'
                     rows={15}
-                    onChange={(e) => setContent(e.target.value)}
+                    onChange={(e) => {
+                        setContent(e.target.value);
+                    }}
                     placeholder="Dear Diary..."
                     spellCheck={false}
                     value={content}
-                />
+                />}
             </div>
             <button
                 className='save button'
                 variant="outline-primary"
                 onClick={() => {
-                    setPost(!post);
-                    console.log(content, startDate);
+                    postNewNotes();
                     setContent('')
                 }}
                 disabled={content.length === 0}>
