4bcdca12a1340f6263a6836e5ed0984816910ed4
Vishnu Rana <mvishnurana@gmail.com>
2022-04-29 16:59:45 +0800

    Updated the date-fn for return the correct format of date

--- diary-ui/src/helper/date-fn.js
+++ diary-ui/src/helper/date-fn.js
@@ -1,4 +1,5 @@
 // Takes new Date() and returns date in format - YYYY-MM-DD
-export function dateFormat(date) {
+export function dateFormat(dateArg) {
+    const date = new Date(dateArg);
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
