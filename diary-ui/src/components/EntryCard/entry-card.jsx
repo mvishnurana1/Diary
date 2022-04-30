a96bb22e34a2c1949d484eb553be2cda74ad36bb
Vishnu Rana <mvishnurana@gmail.com>
2022-04-30 08:45:23 +0800

    Implemented base entry-card component

---
+++ diary-ui/src/components/EntryCard/entry-card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { dateFormat } from '../../helper/date-fn';
+import "./entry-card.scss";
+
+export function EntryCard(props) {
+    const { entries } = props;
+
+    {return  entries.map((entry) => {
+        return (<div className="entry-card-layout" key={entry.entryID}>
+                    <div className="entry-card">
+                        <div className="entry-content">
+                            {entry.content}
+                        </div>
+                        <div className="date-time-log">
+                            <div>
+                                <span className="strong">posted on: </span>
+                                <span className="time">{dateFormat(entry.submittedDateTime)}</span>
+                            </div>
+                            <div>
+                            <FontAwesomeIcon
+                                className='highlight'
+                                icon={faPencil}
+                                size="lg"
+                            />
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+    })}
+}
