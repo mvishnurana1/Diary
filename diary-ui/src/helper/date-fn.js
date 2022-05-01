3f69f3d67e94f7cc264571603d72d8fed95904d0
Vishnu Rana <mvishnurana@gmail.com>
2022-05-01 08:54:08 +0800

    fixed the date-fn, month calculation

--- diary-ui/src/helper/date-fn.js
+++ diary-ui/src/helper/date-fn.js
@@ -1,5 +1,5 @@
 // Takes new Date() and returns date in format - YYYY-MM-DD
 export function dateFormat(dateArg) {
     const date = new Date(dateArg);
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
