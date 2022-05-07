761c5379364f1be0092ba75ce7f504289025b616
Vishnu Rana <mvishnurana@gmail.com>
2022-05-07 23:29:44 +0800

    Used the existing Login component for logged out users

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import { Notes } from './components/Notes/notes';
+import { Login } from './components/Login/login.jsx';
 import './App.css';
 
 function App() {
@@ -12,10 +13,7 @@ function App() {
       {
         isAuthenticated || window.localStorage.getItem("user-verified")
           ? <Notes />
-          : <>
-              <FontAwesomeIcon icon={faBookOpen} />
-              <button onClick={loginWithPopup}>Log in</button>
-            </>
+          : <Login />
       }
     </div>
   );
