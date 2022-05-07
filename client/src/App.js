4b4998f09a7091aa8d7b5c048162abf15e6aa89b
Vishnu Rana <mvishnurana@gmail.com>
2022-05-07 13:28:06 +0800

    Implemented basic oauth on the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import { Notes } from './components/Notes/notes';
 import './App.css';
 
 function App() {
+  const { loginWithPopup, isAuthenticated } = useAuth0();
+
   return (
     <div>
-        <Notes />
+      {
+        isAuthenticated || window.localStorage.getItem("user-verified")
+          ? <Notes />
+          : <>
+              <FontAwesomeIcon icon={faBookOpen} />
+              <button onClick={loginWithPopup}>Log in</button>
+            </>
+      }
     </div>
   );
 }
