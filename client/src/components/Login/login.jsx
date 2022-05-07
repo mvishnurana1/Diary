b18d669997c3283fdb9678908bcf7ea94fb2080e
Vishnu Rana <mvishnurana@gmail.com>
2022-05-07 23:31:43 +0800

    Implemented the Login component

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,10 +1,19 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import './login.scss';
 
-const LoginButton = () => {
+export function Login() {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (<div className="login-page-container">
+    <div className="main-content">
+      <div className="content">
+        <h1 className="header">Diary</h1>
+        <h2 className="text">Daily journaling helps you reflect on your daily choices and benefits your mental health. </h2>
+      </div>
+      <div>
+        <button className="button red" onClick={() => loginWithRedirect()}>Log In / Sign up</button>
+      </div>
+    </div>
+  </div>)
 };
-
-export default LoginButton;
