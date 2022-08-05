192ced18bce2e0662b703f3b3e76998859d3c66f
Vishnu Rana <=>
2022-08-05 17:20:20 +0800

    Implemmented IAuthService and removed IDiaryEntryRepo service

---
+++ API/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using API.DTOs.AuthDto;
+
+namespace API.Helpers.Interfaces
+{
+    public interface IAuthService
+    {
+        void SignUp(SignUpDto signUpDetais);
+        void Login(LoginDto details);
+        void GetLoggedInUser();
+        void DeleteAccount();
+        void Logout();
+    }
+}
