45eb8189fbdb7a50ebdd9ad38035fd2f5e8b670b
Vishnu Rana <=>
2022-08-27 13:02:13 +0800

    Added the IAuthService and the interface

---
+++ API/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Helpers.Interfaces
+{
+    public interface IAuthService
+    {
+        void Login();
+        void Logout();
+        void SignUp();
+    }
+}
---
+++ API/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using API.Helpers.Interfaces;
+
+namespace API.Helpers.Services
+{
+    public class AuthService : IAuthService
+    {
+        public void Login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SignUp()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
