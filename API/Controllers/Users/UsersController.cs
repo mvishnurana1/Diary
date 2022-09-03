57b6032c334f3bbc9cce21e60d34696271766b99
Vishnu Rana <=>
2022-09-03 16:41:24 +0800

    Improved the logs for the API

--- API/Controllers/Users/UsersController.cs
+++ API/Controllers/Users/UsersController.cs
@@ -34,42 +34,48 @@ namespace API.Controllers.Users
         [HttpGet("/getuserbyusername")]
         public async Task<ActionResult<User>> GetUserByUserName([FromQuery] string userName)
         {
-            _logger.LogInformation($"GetUserByUserName Controller method Executed with argument - {userName}");
+            _logger.LogInformation($"GetUserByUserName Controller method Executed with argument - {userName} on {DateTime.Now}");
 
             var user = await _userRespository.FindUserByName(userName);
 
             if (user == null)
             {
+                _logger.LogError($"GetUserByUserName Controller responded with Http-{NotFound().StatusCode}, no user found");
                 return NotFound();
             }
 
+            _logger.LogInformation($"GetUserByUserName responded with Http-{Ok().StatusCode} response - {user}");
             return Ok(user);
         }
 
         [HttpGet("/getallusers")]
         public async Task<ActionResult<IEnumerable<User>>> GetAllUserse()
         {
-            _logger.LogInformation($"GetUserByUserName Controller method Executed with argument");
+            _logger.LogInformation($"GetAllUserse Controller method Executed with argument on {DateTime.Now}");
 
             var user = await _userRespository.GetAllUsers();
 
             var allUsers = _mapper.Map<IEnumerable<User>, IEnumerable<GetAllUsersResponsedDto>>(user);
 
+            _logger.LogInformation($"GetAllUserse responded with Http-{Ok().StatusCode} response - {user}");
             return Ok(allUsers);
         }
 
         [HttpGet("/getuserbyid")]
         public async Task<ActionResult<IEnumerable<User>>> GetUserByUserID([FromQuery] Guid id)
         {
-            _logger.LogInformation($"GetUserByUserName Controller method Executed with argument");
+            _logger.LogInformation($"GetUserByUserID Controller method Executed with argument id - {id} on {DateTime.Now}");
 
             var user = await _userRespository.GetUserByID(id);
 
             if (user == null)
             {
+                _logger.LogInformation($"GetUserByUserID responded with Http-{NotFound().StatusCode} response - {user}");
+
                 return NotFound();
             }
 
+            _logger.LogInformation($"GetUserByUserID responded with Http-{Ok().StatusCode} response - {user}");
             return Ok(user);
         }
     }
