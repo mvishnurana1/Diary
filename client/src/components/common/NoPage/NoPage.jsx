c1044d923d7e42ae4f0028eaefcc06e798a61076
Vishnu Rana <=>
2022-09-03 19:32:29 +0800

    Implemented 404 page

--- client/src/components/common/NoPage/NoPage.jsx
+++ client/src/components/common/NoPage/NoPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceFrown } from "@fortawesome/free-solid-svg-icons";
+import './NoPage.scss';
 
 export function NoPage(){
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <section className='no-page'>
+            <div className='icon-container'>
+                <h1 className='header'>404 - Page Not Found</h1>
+                <FontAwesomeIcon 
+                    icon={faFaceFrown} 
+                    size="2xl"
+                    color='#eb5858'
+                />
+            </div>
+        </section>
     )
 }
