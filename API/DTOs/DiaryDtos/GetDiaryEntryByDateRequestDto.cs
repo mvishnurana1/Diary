d98064660984930e5c50dcb3d1e59d180c64c5d9
Vishnu Rana <=>
2022-09-21 14:40:44 +0800

    Added GetDiaryEntryByDateRequestDto while auth flow is still in progress

---
+++ API/DTOs/DiaryDtos/GetDiaryEntryByDateRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace API.Helpers.Entities
+{
+    public class GetDiaryEntryByDateRequestDto
+    {
+        public Guid UserID { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
