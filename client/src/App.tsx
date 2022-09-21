27271d1015e593205cb5296f5951e1929ae20d9d
Vishnu Rana <=>
2022-09-21 22:42:30 +0800

    Removed the redundant routes from the route declaration and deleted the implementation of the components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes } from 'react-router';
 import { Notes } from './components/Notes/notes';
 import { Login } from './components/Login/login';
-import { Preferences } from './components/Preferences/Preferences';
-import { Statistics } from './components/Statistics/Statistics';
 import { NoPage }  from './components/common/NoPage/NoPage';
 import { Header } from './components/common/Header/Header';
 import './App.scss';
@@ -15,8 +13,6 @@ function App(): JSX.Element {
         <Routes>
             <Route path="" element={<Login />} />
             <Route path="new" element={<Notes />} />
-            <Route path="preferences" element={<Preferences />} />
-            <Route path="stats" element={<Statistics />} />
             <Route path="*" element={<NoPage />} />
         </Routes>
       </Router>
