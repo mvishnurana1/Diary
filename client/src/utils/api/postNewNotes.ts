c54b6396033582d6e056e74d9aad007496b1f850
Vishnu Rana <=>
2022-10-01 15:16:32 +0800

    moved the API call for posting/fetching entries

---
+++ client/src/utils/api/postNewNotes.ts
@@ -0,0 +1,19 @@
+import { GetToken } from "../../helper/getToken";
+import { DiaryEntry } from "../../models/DiaryEntry";
+import { postNewDiaryEntryModel } from "../../models/PostNewDiaryEntryModel";
+import { BASE_URL } from "../url";
+
+export async function postNewNotes(newNote: postNewDiaryEntryModel): Promise<DiaryEntry> {
+    const token = GetToken();
+
+    const response = await fetch(`${BASE_URL}post/`, {
+        method: 'POST',
+        body: JSON.stringify(newNote),
+        headers: {
+            'Authorization': `bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return await response.json() as Promise<DiaryEntry>;
+}
