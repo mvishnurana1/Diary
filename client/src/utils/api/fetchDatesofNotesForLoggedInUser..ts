d1f123b120bed1b4285286f0a72043bf2e486145
Vishnu Rana <=>
2022-10-08 20:31:36 +0800

    Implemented API call fetchDatesofNotesForLoggedInUser

---
+++ client/src/utils/api/fetchDatesofNotesForLoggedInUser..ts
@@ -0,0 +1,20 @@
+import { GetToken } from "../../helper/getToken";
+import { BASE_URL } from "../url";
+
+export async function fetchDatesofNotesForLoggedInUser(loggedInUserID: string) {
+    if (!loggedInUserID) {
+        return;
+    }
+
+    const token = GetToken();
+
+    const response = await fetch(`${BASE_URL}getdates/?id=${loggedInUserID}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return await response.json() as Promise<Date[]>;
+}
