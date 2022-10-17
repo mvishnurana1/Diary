8eb6b463d8730e5dec92954e3387093cca9bd29b
Vishnu Rana <=>
2022-10-17 19:33:50 +0800

    Implemented the component that performs CRUD operations on the component

--- client/src/components/MonthGoal/monthGoal.tsx
+++ client/src/components/MonthGoal/monthGoal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullseye, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { getMonthTitle } from "../../helper/date-fn";
@@ -12,20 +12,58 @@ const monthlyGoal = [{
     content: 'some vusavp mnweorivn jkw bijprwdebn v kjdj vbwirebvi eijwv bepq'
 }];
 
+type defaultGoal = {
+    content: string
+    isEditting: boolean,
+    index: number
+}
+
+const defaultEdit: defaultGoal = {
+    content: '',
+    isEditting: false,
+    index: -1
+}
+
 export function MonthGoal(): JSX.Element {
-    return <div className="month-goal">
+    const [ edit, setEdit ] = useState<defaultGoal>(defaultEdit);
+    const [ edittedContent,  setEdittedContent] = useState('');
+
+    function editGoal(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) {
+        const content = monthlyGoal[index].content;
+        monthlyGoal.splice(0, 1, { content: edittedContent });
+        setEdit({ isEditting: !edit.isEditting, index: index, content: edittedContent });
+        setEdittedContent('');
+    }
+
+    return (
+    <div className="month-goal">
         <h1 className="title top">{getMonthTitle()} priorities</h1>
         <div className="goals-group-container">
             {monthlyGoal.map((goal, index) => 
                 <div className="flex-gap-row goal-item" key={index}>
                     <div className="flex-gap-row">
-                        <div>
+                        {edit.isEditting && edit.index === index ? null : <div>
                             <FontAwesomeIcon className="goal-logo" icon={ faBullseye } />
-                        </div>
-                        <div className="content">{goal.content}</div>
+                        </div>}
+                        {
+                            (edit.isEditting && edit.index === index) 
+                            ? <input 
+                                className='update-input' 
+                                type="text" 
+                                onChange={e => setEdittedContent(e.target.value)}
+                              /> 
+                            : <div className="content">{goal.content}</div>
+                        }
                     </div>
                     <div className="edit-panel">
-                        <button className="edit-button" title="edit" >
+                        <button 
+                            className="edit-button" 
+                            title="edit"
+                            onClick={(event) => {
+                                editGoal(event, index);
+                                // monthlyGoal.splice();
+                                // setEdit({ content: monthlyGoal[index].content, index, isEditting: !edit.isEditting }); 
+                            }}>
                             <FontAwesomeIcon className="goal-edit" icon={ faPen } />
                         </button>
                         <button className="edit-button" title="delete">
@@ -35,5 +73,5 @@ export function MonthGoal(): JSX.Element {
                 </div>
             )}
         </div>
-    </div>
+    </div>)
 }
