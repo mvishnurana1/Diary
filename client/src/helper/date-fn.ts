1c04167c46ee92a6793d0ee5e6ec2b904561d4ab
Vishnu Rana <=>
2022-10-17 19:59:57 +0800

    Improved the implementation of the getMonthsTitle function

--- client/src/helper/date-fn.ts
+++ client/src/helper/date-fn.ts
@@ -5,44 +5,13 @@ export const dateFormat = (dateArg: Date): string => {
 }
 
 export const getMonthTitle = (): string => {
-    const month = (new Date().getMonth() + 1);
+    const months = [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August', 'September', 
+        'October', 'November', 'December'
+    ];
     
-    if (month === 1) {
-        return 'January';
-    }
-    if (month === 2) {
-        return 'February';
-    }
-    if (month === 3) {
-        return 'March';
-    }
-    if (month === 4) {
-        return 'April';
-    }
-    if (month === 5) {
-        return 'May';
-    }
-    if (month === 6) {
-        return 'June';
-    }
-    if (month === 7) {
-        return 'July';
-    }
-    if (month === 8) {
-        return 'August';
-    }
-    if (month === 9) {
-        return 'September';
-    }
-    if (month === 10) {
-        return 'October';
-    }
-    if (month === 11) {
-        return 'November';
-    }
-    if (month === 12) {
-        return 'December';
-    } else {
-        return '';
-    }
+    const month = (new Date().getMonth());
+    
+    return months[month];
 }
