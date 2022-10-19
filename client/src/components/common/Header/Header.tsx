b15d062962806eab673c2f56f061d1ee8a78b9ed
Vishnu Rana <=>
2022-10-19 12:28:21 +0800

    Leveraged the props passed down by Notes to avoid additional API calls to Oauth0

--- client/src/components/common/Header/Header.tsx
+++ client/src/components/common/Header/Header.tsx
@@ -1,47 +1,52 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { LogoutOptions, User } from '@auth0/auth0-react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './Header.scss';
 
-export function Header(): JSX.Element {
-    const { logout, isAuthenticated, user } = useAuth0();
+interface headerProps {
+    logout: (options?: LogoutOptions | undefined) => void,
+    user: User | undefined
+}
+
+export function Header(props: headerProps): JSX.Element {
+    const { logout, user } = props;
     
+    if (!props) {
+        return <div></div>;
+    }
+
     return (
-    <>
-        {isAuthenticated && user &&
-            <div className="header-container layout">
-                <div className="logout-btn-container">
-                    <Dropdown>
-                        <Dropdown.Toggle
-                            className='logout'
-                            id="dropdown-basic">
-                            <img
-                                alt=""
-                                className="logo"
-                                src={require('../../../assets/user.png')}
-                                title={user?.nickname}
-                            />
-                            {user?.nickname}
-                        </Dropdown.Toggle>
+        <div className="header-container layout">
+            <div className="logout-btn-container">
+                <Dropdown>
+                    <Dropdown.Toggle
+                        className='logout'
+                        id="dropdown-basic">
+                        <img
+                            alt=""
+                            className="logo"
+                            src={require('../../../assets/user.png')}
+                            title={user?.nickname}
+                        />
+                        {user?.nickname}
+                    </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item 
-                                onClick={() => {
-                                    window.localStorage.removeItem('accessToken');
-                                    window.localStorage.removeItem('email');
-                                    window.localStorage.removeItem('idToken');
-                                    window.localStorage.removeItem('photo');
+                    <Dropdown.Menu>
+                        <Dropdown.Item 
+                            onClick={() => {
+                                window.localStorage.removeItem('accessToken');
+                                window.localStorage.removeItem('email');
+                                window.localStorage.removeItem('idToken');
+                                window.localStorage.removeItem('photo');
 
-                                    logout({
-                                        returnTo: window.location.origin,
-                                    })
-                                }}
-                            >
-                                Log Out
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
+                                logout({
+                                    returnTo: window.location.origin,
+                                })
+                            }}
+                        >
+                            Log Out
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
-        }
-    </>)
+        </div>)
 }
