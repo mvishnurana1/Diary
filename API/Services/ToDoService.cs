4fd59cc01525163669ef9152be47ec3fd7309ed9
Vishnu Rana <=>
2022-10-21 01:42:40 +0800

    WIP: Implemented the basic serivces for the functionality

---
+++ API/Services/ToDoService.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Helpers.Services
+{
+    public interface IToDoService
+    {
+
+    }
+
+    public class ToDoService : IToDoService
+    {
+
+    }
+
+    public interface IPrioritiesService
+    {
+
+    }
+
+    public class PrioritiesService : IPrioritiesService
+    {
+
+    }
+}
