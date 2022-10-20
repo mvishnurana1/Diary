b8194538a83f3827599f1d537ce6b79c473d48e9
Vishnu Rana <=>
2022-10-20 23:24:03 +0800

    Implemented the Priority model

---
+++ API/Models/Priority.cs
@@ -0,0 +1,17 @@
+﻿namespace API.model
+{
+    public class Priority
+    {
+        [Key]
+        public Guid ID { get; set; } = new Guid();
+
+        [Required]
+        public Guid UserID { get; set; }
+
+        [Required]
+        public DateTime Month { get; set; }
+
+        [Required]
+        public string PriorityContent { get; set; }
+    }
+}
