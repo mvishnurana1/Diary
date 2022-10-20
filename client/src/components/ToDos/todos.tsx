6ca7a1b8a7777413ae7eb3e2765470d2b26e236f
Vishnu Rana <=>
2022-10-21 07:55:30 +0800

    fixed the flow of saving content to the todo list state

--- client/src/components/ToDos/todos.tsx
+++ client/src/components/ToDos/todos.tsx
@@ -1,29 +1,33 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import './todos.scss';
 
 interface UserTask {
     id: string,
     content: string,
-    date: Date
+    date: Date,
+    isCompleted: false
 }
 
 const defaultTask : UserTask = {
     id: '',
     content: '',
-    date: new Date()
+    date: new Date(),
+    isCompleted: false
 }
 
 export function ToDos(): JSX.Element {
     const defaultToDos: UserTask[] = [{
         id: '1',
         content: 'some bullshit',
-        date: new Date('2022-10-19')
+        date: new Date('2022-10-19'),
+        isCompleted: false
     }, {
         id: '2',
         content: 'some more bullshit',
-        date: new Date('2022-10-19')
+        date: new Date('2022-10-19'),
+        isCompleted: false
     }];
 
     const [todos, setToDos] = useState(defaultToDos);
@@ -49,11 +53,13 @@ export function ToDos(): JSX.Element {
 
                         {isAdding && <input className="gap input" onChange={(e) => { 
                                 const value = e.target.value.trim();
+                                if (value.length <= 0) {
+                                    return;
+                                }
 
                                 if (value.length > 0) {
-                                    const newToDo: UserTask = { id: '100', content: value, date: new Date() };
+                                    const newToDo: UserTask = { id: '100', content: value, date: new Date(), isCompleted: false };
                                     setActive(newToDo)
-                                    setToDos([...todos, newToDo]);
                                 }}
                             }
                         />}
@@ -63,6 +69,13 @@ export function ToDos(): JSX.Element {
                         title="Add"
                         onClick={() => {
                             setIsAdding(!isAdding);
+
+                            const activeContent = active.content.trim();
+
+                            if (activeContent.length > 0) {
+                                setToDos([...todos, active]);
+                                setActive(defaultTask);
+                            }
                         }}
                     >
                         <FontAwesomeIcon icon={ faPlus } />
