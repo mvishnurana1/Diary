a276f30af4203dd6f6e420088bf3a47ad658d9c0
Vishnu Rana <=>
2022-10-21 18:06:18 +0800

    Implemented the monthly performance

---
+++ client/src/components/PerformanceChart/performanceChart.tsx
@@ -0,0 +1,9 @@
+import './performanceChart.scss';
+
+export function PerformanceChart(): JSX.Element {
+    return (<div className='chart-section'>
+        <h1 className='title'>Performance</h1>
+        <h2 className='sub-title'>this month</h2>
+        <hr />
+    </div>)
+}
