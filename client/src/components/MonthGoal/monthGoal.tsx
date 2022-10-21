3d8bc266addb4f4732c68d4167f071add6cf8788
Vishnu Rana <=>
2022-10-21 22:23:15 +0800

    Implemented the first draft of the monthGoals component

--- client/src/components/MonthGoal/monthGoal.tsx
+++ client/src/components/MonthGoal/monthGoal.tsx
@@ -1,86 +1,97 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBullseye, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faBullseye, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { getMonthTitle } from "../../helper/date-fn";
+import { UserTask } from "../../models/UserTask";
 import './monthGoal.scss';
 
-const monthlyGoal = [{
-    content: 'Item number 1'
-}, {
-    content: 'x'
-}, {
-    content: 'some vusavp mnweorivn jkw bijprwdebn v kjdj vbwirebvi eijwv bepq'
-}];
-
-type defaultGoal = {
-    content: string
-    isEditting: boolean,
-    index: number
-}
-
-const defaultEdit: defaultGoal = {
+const defaultTask : UserTask = {
     content: '',
-    isEditting: false,
-    index: -1
+    date: new Date(),
+    isCompleted: false
 }
 
 export function MonthGoal(): JSX.Element {
-    const [ edit, setEdit ] = useState<defaultGoal>(defaultEdit);
-    const [ edittedContent,  setEdittedContent] = useState('');
-
-
-    function editGoal(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) {
-        monthlyGoal.splice(index, 1, { content: edittedContent });
-        setEdit({ isEditting: !edit.isEditting, index: index, content: edittedContent });
-        setEdittedContent('');
-    }
-
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const { id, value  } = e.target;
-
-        setEdittedContent(value);
+    const [todos, setToDos] = useState<UserTask[]>(getExistingTodos());
+    const [isAdding, setIsAdding] = useState(false);
+    const [active, setActive] = useState<UserTask>(defaultTask);
+    
+    function getExistingTodos(): UserTask[] {
+        const tasks: UserTask[] = JSON.parse(localStorage.getItem('priorities')!);
+        return tasks === null ? [] : tasks;
     }
 
     return (
-    <div className="month-goal">
-        <h1 className="title">
-            <div>{getMonthTitle()}'s</div> 
-            <div className='sub-title'>top priorities</div>
-        </h1>
-        <div className="goals-group-container">
-            {monthlyGoal.map((goal, index) => 
-                <div className="flex-gap-row goal-item" key={index}>
-                    <div className="flex-gap-row">
-                        {edit.isEditting && edit.index === index ? null : <div>
-                            <FontAwesomeIcon className="goal-logo" icon={ faBullseye } />
-                        </div>}
-                        {
-                            (edit.isEditting && edit.index === index) 
-                            ? <input 
-                                className='update-input' 
-                                type="text"
+        <section className="todos-section month-goal">
+            <div>
+                <h1 className="title">{getMonthTitle()}'s</h1>
+                    <h2 className="sub-title">top priorities</h2>
+                    <div className="gap">
+                        {todos?.length === 0 && <span>Nothing to prioritise?</span>}
+                        {todos?.length > 0 && todos.map((todo, index) => 
+                            <div className="todo-item" key={index}>
+                                <FontAwesomeIcon className="goal-logo" icon={ faBullseye } />
 
-                                onChange={e => handleChange(e)}
-                              /> 
-                            : <div className="content">{goal.content}</div>
+                                <div className="flex-gap">
+                                    <div className={todo.isCompleted ? 'content isCompleted': 'content'}>
+                                        {todo.content}
+                                    </div>
+                                    <button 
+                                        className="trash-button" 
+                                        onClick={() => {
+                                            todos.splice(index, 1);
+                                            setToDos([...todos]);
+                                            localStorage.setItem('priorities', JSON.stringify([...todos]));
+                                        }}
+                                        title="remove">
+                                        <FontAwesomeIcon icon={ faTrash } />
+                                    </button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                    {isAdding && 
+                        <input 
+                            className="gap input" 
+                            onChange={(e) => { 
+                                    const value = e.target.value.trim();
+                                    if (value.length <= 0) {
+                                        return;
+                                    }
+
+                                    if (value.length > 0) {
+                                        const newToDo: UserTask = { 
+                                            content: value, 
+                                            date: new Date(), 
+                                            isCompleted: false
+                                        };
+                                        setActive(newToDo)
+                                    }}
+                            }
+                            placeholder="write new task here"
+                    />}
+
+                <button 
+                    className="logo-button centre" 
+                    title="Add"
+                    onClick={() => {
+                        setIsAdding(!isAdding);
+
+                        const activeContent = active.content.trim();
+
+                        if (activeContent.length > 0) {
+                            setToDos([...todos, active]);
+                            localStorage.setItem('priorities', JSON.stringify([...todos, active]));
+                            setActive(defaultTask);
                         }
-                    </div>
-                    <div className="edit-panel">
-                        <button 
-                            className="edit-button" 
-                            title="edit"
-                            onClick={(event) => {
-                                editGoal(event, index);
-                            }}>
-                            <FontAwesomeIcon className="goal-edit" icon={ faPen } />
-                        </button>
-                        <button className="edit-button" title="delete">
-                            <FontAwesomeIcon className="goal-delete" icon={ faTrash } />
-                        </button>
-                    </div>
-                </div>
-            )}
-        </div>
-        <hr />
-    </div>)
+                    }}
+                >
+                    <FontAwesomeIcon icon={ faPlus } />
+                    <div>Add</div>
+                </button>
+            </div>
+            <hr />
+        </section>
+    )
 }
