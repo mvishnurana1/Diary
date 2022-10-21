9d2220eaa76dbd9937ffb208634a4d1b02faf2ff
Vishnu Rana <=>
2022-10-21 23:34:38 +0800

    Indented the rules of chart scales and plugins

--- client/src/components/PerformanceChart/performanceChart.tsx
+++ client/src/components/PerformanceChart/performanceChart.tsx
@@ -25,15 +25,11 @@ export function PerformanceChart(): JSX.Element {
     const options = {
         responsive: true,
         plugins: {
-            legend: {
-                display: false
-            },
+            legend: { display: false },
             data: {
                 labels: { display: false }
             },
-            title: {
-                display: false,
-            }
+            title: { display: false }
         },
         scales: {
             y: {
@@ -53,10 +49,9 @@ export function PerformanceChart(): JSX.Element {
         labels,
         datasets: [
           {
-            label: `${labels[new Date().getMonth()]}`,
             data: [122, 52, 82, 12, 42, 62, 12, 42, 72, 32, 2, 42, 12, 2, 142, 22, 62, 142, 128, 65, 21, 92, 72, 32, 92, 32, 72, 62, 122, 21],
-            borderColor: "rgb(255, 99, 132)",
-            backgroundColor: "rgba(255, 99, 132, 0.5)"
+            borderColor: "rgb(235, 88, 88)",
+            backgroundColor: "rgb(235, 88, 88)"
           }
         ]
     };
