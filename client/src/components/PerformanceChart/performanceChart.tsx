b3ee7e4980320cfdcdc2a1cccd8979182ee3e24e
Vishnu Rana <=>
2022-10-21 22:44:53 +0800

    Implemented a chart on the component

--- client/src/components/PerformanceChart/performanceChart.tsx
+++ client/src/components/PerformanceChart/performanceChart.tsx
@@ -1,9 +1,73 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  } from "chart.js";
+import { Line } from "react-chartjs-2";
 import './performanceChart.scss';
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
 export function PerformanceChart(): JSX.Element {
-    return (<div className='chart-section'>
+    const options = {
+    responsive: true,
+    plugins: {
+            legend: {
+                display: false
+            },
+            data: {
+                labels: { display: false }
+            },
+                title: {
+                display: true,
+                text: "Chart.js Line Chart"
+            }
+        }
+    };
+
+    const labels = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct"
+    ];
+    
+    const data = {
+        labels,
+        datasets: [
+          {
+            label: `${labels[new Date().getMonth()]}`,
+            data: labels.map(() => [1, 100]),
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)"
+          }
+        ]
+    };
+    
+    return (
+    <div className='chart-section'>
         <h1 className='title'>Performance</h1>
         <h2 className='sub-title'>this month</h2>
+        <Line data={data} options={options} />;
         <hr />
     </div>)
 }
