76941ff0e94a98e4106973efcb02bfe325bc144e
Vishnu Rana <=>
2022-10-26 00:47:25 +0800

    Implemented the Priorities related Dtos

---
+++ API/DTOs/Priority/PriorityResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace API.DTOs.Priority
+{
+    public class PriorityResponseDto
+    {
+        public Guid ID { get; set; }
+        public string PriorityContent { get; set; }
+    }
+
+    public class PriorityDto
+    {
+        public Guid UserID { get; set; }
+        public DateTime Month { get; set; }
+        public string PriorityContent { get; set; }
+    }
+}
