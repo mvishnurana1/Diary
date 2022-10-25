d3823421047f1c330529aacb23e168114b2380b3
Vishnu Rana <=>
2022-10-25 17:33:19 +0800

    Implemented ToDoService

--- API/Services/ToDoService.cs
+++ API/Services/ToDoService.cs
@@ -1,22 +1,48 @@
-﻿namespace API.Helpers.Services
+﻿using API.model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Helpers.Services
 {
-    public interface IToDoService
+    public interface ITaskService
     {
-
+        Task<IEnumerable<DailyTodo>> AddTodaysTask(string content);
+        Task<IEnumerable<DailyTodo>> RemoveTodaysTask(string content);
     }
 
-    public class ToDoService : IToDoService
+    public class ToDoService : ITaskService
     {
+        public async Task<IEnumerable<DailyTodo>> AddTodaysTask(string content)
+        {
+            var x = new List<DailyTodo>();
+            return await Task.Run(() => x);
+        }
 
+        public async Task<IEnumerable<DailyTodo>> RemoveTodaysTask(string content)
+        {
+            var x = new List<DailyTodo>();
+            return await Task.Run(() => x);
+        }
     }
 
-    public interface IPrioritiesService
+    public interface IMonthPriority
     {
-
+        Task<IEnumerable<Priority>> AddMonthPriority(string content);
+        Task<IEnumerable<Priority>> RemoveMonthPriority(string content);
     }
 
-    public class PrioritiesService : IPrioritiesService
+    public class MonthPrioritiesService : IMonthPriority
     {
+        public async Task<IEnumerable<Priority>> AddMonthPriority(string content)
+        {
+            var x = new List<Priority>();
+            return await Task.Run(() => x);
+        }
 
+        public async Task<IEnumerable<Priority>> RemoveMonthPriority(string content)
+        {
+            var x = new List<Priority>();
+            return await Task.Run(() => x);
+        }
     }
 }
