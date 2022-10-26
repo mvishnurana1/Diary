3e57190304cb38b6cb5fe1f03b7882ec7eb0b963
Vishnu Rana <=>
2022-10-26 12:01:12 +0800

    Implemented the priorityToPriorityResponse profile

---
+++ API/Mappings/Priorities/priorityToPriorityResponse.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs.Priority;
+using API.model;
+using AutoMapper;
+
+namespace API.Mappings.Priorities
+{
+    public class priorityToPriorityResponse : Profile
+    {
+        public priorityToPriorityResponse()
+        {
+            CreateMap<Priority, PriorityResponseDto>();
+        }
+    }
+}
