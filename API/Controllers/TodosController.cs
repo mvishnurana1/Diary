346bea769724fa2992a5f4674a9c01f7a5a999e7
Vishnu Rana <=>
2022-12-30 20:47:30 +0800

    WIP: Implemented the TodosController

---
+++ API/Controllers/TodosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Users
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodosController : ControllerBase
+    {
+
+    }
+}
