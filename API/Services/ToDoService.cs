24be4acd12f1828d8dc111b3fb0d928b1ddabd52
Vishnu Rana <=>
2022-12-30 18:29:12 +0800

    Implemented Todo Service and related interface

--- API/Services/ToDoService.cs
+++ API/Services/ToDoService.cs
@@ -1,22 +1,25 @@
-﻿namespace API.Helpers.Services
+﻿using API.DTOs.Todos;
+using API.model;
+using System.Threading.Tasks;
+
+namespace API.Helpers.Services
 {
     public interface IToDoService
     {
-
+        Task<DailyTodo> AddNewTodo(TodoDto todo);
+        Task<string> DeleteTodo(DeleteTodoRequestDto deleteTodoRequestDto);
     }
 
     public class ToDoService : IToDoService
     {
+        public Task<DailyTodo> AddNewTodo(TodoDto todo)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    }
-
-    public interface IPrioritiesService
-    {
-
-    }
-
-    public class PrioritiesService : IPrioritiesService
-    {
-
+        public Task<string> DeleteTodo(DeleteTodoRequestDto deleteTodoRequestDto)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
