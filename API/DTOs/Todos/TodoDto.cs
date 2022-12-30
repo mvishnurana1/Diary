778613c3c8e4bce851d9cecafc0db170a6c430ae
Vishnu Rana <=>
2022-12-30 18:20:33 +0800

    Implemented TodoDto model and DeleteTodoRequestDto model

---
+++ API/DTOs/Todos/TodoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace API.DTOs.Todos
+{
+    public class TodoDto
+    {
+        public DateTime DateDue { get; set; }
+        public bool Completed { get; set; }
+        public Guid UserID { get; set; }
+        public string TodoContent { get; set; }
+    }
+
+    public class DeleteTodoRequestDto 
+    {
+        public Guid ID { get; set; }
+        public Guid loggedInUserID { get; set; }
+    }
+}
