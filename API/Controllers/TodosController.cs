0ccba33710a63a274c0b049abde62ff6dcedc0b9
Vishnu Rana <=>
2022-12-31 18:03:42 +0800

    WIP: Implemented the TodoController

--- API/Controllers/TodosController.cs
+++ API/Controllers/TodosController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API.Helpers.Services;
+using API.model;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.Controllers.Users
 {
@@ -8,6 +14,37 @@ namespace API.Controllers.Users
     [Route("api/[controller]")]
     public class TodosController : ControllerBase
     {
+        private readonly ILogger<DiaryEntryController> _logger;
+        private readonly IToDoService _toDoService;
+        private readonly IMapper _mapper;
 
+        public TodosController(
+            IToDoService toDoService,
+            IMapper mapper,
+            ILogger<DiaryEntryController> logger
+        )
+        {
+            _toDoService = toDoService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpGet("activetodos")]
+        public async Task<ActionResult<List<DailyTodo>>> GetActiveTodos([FromQuery] string userID)
+        {
+
+        }
+
+        [HttpPost("addtodo")]
+        public async Task<ActionResult<List<DailyTodo>>> AddNewTodo()
+        {
+
+        }
+
+        [HttpPut("updatetodo")]
+        public async Task<ActionResult<List<DailyTodo>>> GetActiveTodos()
+        {
+
+        }
     }
 }
