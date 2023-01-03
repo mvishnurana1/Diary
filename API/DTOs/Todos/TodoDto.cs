6e34b2bcde478c804c294da2ec5fc0eb290d7591
Vishnu Rana <=>
2023-01-03 08:14:48 +0800

    Removed completed property from the TodoDto and set the default value to false

--- API/DTOs/Todos/TodoDto.cs
+++ API/DTOs/Todos/TodoDto.cs
@@ -5,7 +5,6 @@ namespace API.DTOs.Todos
     public class TodoDto
     {
         public DateTime DateDue { get; set; }
-        public bool Completed { get; set; }
         public Guid UserID { get; set; }
         public string TodoContent { get; set; }
     }
