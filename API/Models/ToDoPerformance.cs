01cb3dc81e379fe867ca071023129bb78364e4a2
Vishnu Rana <=>
2023-01-04 08:25:23 +0800

    Changed Achievement property from decimal to Double

--- API/Models/ToDoPerformance.cs
+++ API/Models/ToDoPerformance.cs
@@ -6,6 +6,6 @@ namespace API.model
     {
         public Guid ID { get; set; } = new Guid();
         public DateTime? Date { get; set; }
-        public Decimal? Achievement { get; set; }
+        public Double? Achievement { get; set; }
     }
 }
