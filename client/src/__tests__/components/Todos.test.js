f85e5ef9371aea92ea7b6c8774b6e6296f300223
vish <mvishnurana@gmail.com>
2023-09-07 20:48:49 +0800

    removed redundant import of React

--- client/src/__tests__/components/Todos.test.js
+++ client/src/__tests__/components/Todos.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import ToDos from '../../components/ToDos/todos.tsx';
