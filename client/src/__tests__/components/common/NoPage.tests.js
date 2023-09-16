8120df61366f381ac2a1510eb1bfa45beac25a26
vish <mvishnurana@gmail.com>
2023-09-16 20:33:09 +0800

    Improved tests by removed redundant act

--- client/src/__tests__/components/common/NoPage.tests.js
+++ client/src/__tests__/components/common/NoPage.tests.js
@@ -1,16 +1,16 @@
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { NoPage } from '../../../components/common/NoPage/NoPage.tsx';
 
 describe('No Page Component', () => {
     test('renders the error message successfully', () => {
-        act(() => render(<NoPage />));
+        render(<NoPage />);
     
         const errorTextElement = screen.getByText('404 - Page Not Found');
         expect(errorTextElement).toBeInTheDocument();
     });
     
     test('renders the FontAwesomeIcon compponent with icon', () => {
-        act(() => render(<NoPage />));
+        render(<NoPage />);
 
         const fontAwesomeIcon = screen.getByTitle("404");
         expect(fontAwesomeIcon).toBeInTheDocument();
