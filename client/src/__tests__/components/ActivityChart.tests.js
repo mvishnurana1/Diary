042faa69c4aae8421cc3fe827dfa60823bd9e08a
vish <mvishnurana@gmail.com>
2023-09-16 20:30:14 +0800

    Added tests for the component

---
+++ client/src/__tests__/components/ActivityChart.tests.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import PerformanceChart from "../../../src/components/ActivityChart/ActivityChart.tsx";
+
+
+// To fix the warning from the ActivityChart Component
+jest.mock('react-chartjs-2', () => ({
+    ...jest.requireActual('react-chartjs-2'),
+    Line: jest.fn(() => <div>LINE</div>),
+}));
+
+jest.mock('react-chartjs-2', () => ({
+    ...jest.requireActual('react-chartjs-2'),
+    Line: jest.fn(() => <div>LINE</div>),
+}));
+
+describe("PerformanceChart Component:", () => {
+    test("Displays the header h1 with text 'Activity'", async () => {
+        render(<PerformanceChart />);
+        const header = await screen.findByText("Activity");
+
+        expect(header.innerHTML).toBe("Activity");
+    });
+
+    test("Displays the header h2 with text 'this month'", async () => {
+        render(<PerformanceChart />);
+        const subHeader = await screen.findByText("this month");
+
+        expect(subHeader.innerHTML).toBe("this month");
+    });
+});
