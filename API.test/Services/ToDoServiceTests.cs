4ef92c14d6df9e8174c7be1a6fd8ef8555f2f10e
vish <mvishnurana@gmail.com>
2023-09-19 16:42:58 +0800

    Added new project for API testing using xUnit

--- API.test/Services/ToDoServiceTests.cs
+++ API.test/Services/ToDoServiceTests.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using API.DTOs.Todos;
+using API.Helpers.Services;
 
 namespace API.test.Services
 {
     public class ToDoServiceTests
     {
         [Fact]
-        public void GivenToDoIsNotDuplicate_WhenToDoIsAdded_ItIsStoredCorrectly()
+        public async Task GivenToDoIsNotDuplicate_WhenToDoIsAdded_ItIsStoredCorrectly()
         {
+            var todoService = new ToDoService();
 
-        }
+            var dto = new TodoDto();
 
-        [Fact]
-        public void GivenToDoContentIsFoundForUser_ForDate_WhenTheToDoIsAdded_ItIsNotStored()
-        {
-
+            var x = await todoService.AddNewTodo(dto);
+            
         }
 
         [Fact]
