3bc8d6c3aabcda04a2601aa3bbf0164be4532715
vish <mvishnurana@gmail.com>
2023-10-27 09:21:23 +0800

    Implemented NotesContext and Provider

--- client/src/context/notes/NotesContext.ts
+++ client/src/context/notes/NotesContext.ts
@@ -1,10 +1,13 @@
 import { createContext } from "react";
+import { DiaryEntry } from "../../models";
 
 const NotesContext = createContext({
-    consent: String,
-    names: Array<string>,
-    setNames: (name: string) => {},
-    setConsent: (val: string) => {}
+    content: '', setContent: (val: string) => {},
+    startDate: new Date(), setStartDate: (date: Date) => {},
+    error: false, setError: (errorVal: boolean) => {},
+    recentlyPosted: false, setRecentlyPosted: (val: boolean) => {},
+    searchedResult: [], setSearchedResult: (entries: DiaryEntry[]) => {},
+    validNoteDates: [], setValidNoteDates: (newDates: Array<Date>) => {}
 });
 
 export default NotesContext;
--- client/src/context/notes/NotesProvider.jsx
+++ client/src/context/notes/NotesProvider.jsx
@@ -1,27 +1,26 @@
-import { createContext, useState } from "react";
-
-const NotesContext = createContext({
-    consent: '',
-    names: [],
-    setNames: (...params) => {},
-    setConsent: (...params) => {}
-});
+import { useState } from "react";
+import NotesContext from './NotesContext';
 
 function NotesProvider({children}) {
-    // const [contentInTextBox, setContentInTextBox] = useState('');
-    const [consent, setConsent] = useState("Yes");
-    const [names, setNames] = useState([]);
+    const [error, setError] = useState(false);
+    const [validNoteDates, setValidNoteDates] = useState([]);
+    const [content, setContent] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [recentlyPosted, setRecentlyPosted] = useState(false);
+    const [searchedResult, setSearchedResult] = useState([]);
 
     return (
         <NotesContext.Provider value={{
-                names,
-                consent,
-                setNames,
-                setConsent
-            }}>
+            content, setContent,
+            startDate, setStartDate,
+            error, setError,
+            recentlyPosted, setRecentlyPosted,
+            searchedResult, setSearchedResult,
+            validNoteDates, setValidNoteDates,
+        }}>
             {children}
         </NotesContext.Provider>
     )
 }
 
-export { NotesContext, NotesProvider };
+export { NotesProvider };
