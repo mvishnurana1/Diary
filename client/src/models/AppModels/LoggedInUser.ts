779d7fd7f065e853e8ad39be7e63747ae5d95da0
vish <mvishnurana@gmail.com>
2023-11-02 16:52:27 +0800

    Updated model

--- client/src/models/AppModels/LoggedInUser.ts
+++ client/src/models/AppModels/LoggedInUser.ts
@@ -2,6 +2,9 @@ interface LoggedInUser {
     email: string,
     userID: string
     userName: string,
+    picture: string,
+    nickname: string,
+    logout: ({...params}) => void
 }
 
 export { type LoggedInUser };
