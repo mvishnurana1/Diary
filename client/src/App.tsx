811b23e367400aeda5f47b4d94104db48aef8f1c
vish <mvishnurana@gmail.com>
2023-11-02 16:26:54 +0800

    Used AuthProvider in the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import { Routes } from 'react-router';
 import { Notes } from './components/Notes/notes';
 import { LandingPage } from './components/LandingPage/landingPage';
 import { NoPage }  from './components/common';
+import AuthProvider from "./context/AuthProvider/auth/AuthProvider";
 import './App.scss';
 
 function App(): JSX.Element {
   return (
+    <AuthProvider>
       <Router>
         <Routes>
             <Route path="" element={<LandingPage />} />
@@ -14,6 +16,7 @@ function App(): JSX.Element {
             <Route path="*" element={<NoPage />} />
         </Routes>
       </Router>
+    </AuthProvider>
   )
 }
 
